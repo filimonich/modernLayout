0ce7d929a685dfe2fc5269cb0eabe57719cc3579
Sergey Filimonov <srfilimonoff@gmail.com>
2023-04-24 15:13:14 +0300

    to do home task 4

--- vite/native/vite.config.js
+++ vite/native/vite.config.js
@@ -5,6 +5,12 @@ export default defineConfig({
   build: {
     outDir: "./../docs",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "/index.html", // путь к главному HTML-файлу
+        other: "./src/other/other.html", // путь к файлу other.html
+      },
+    },
   },
   publicDir: "./../public",
 });
